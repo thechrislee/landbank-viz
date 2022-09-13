be114a8139ed4b6eee1463ce6eb0ef0ef9316ce4
Chris Lee <chris@noreply.github.com>
2022-09-13 09:01:46 -0400

    initial commit of cmd line app.

---
+++ src/landbank_viz/__main__.py
@@ -0,0 +1,15 @@
+""""""
+import typer
+from .scrape import Scrape
+
+cli = typer.Typer()
+
+@cli.command()
+def scrape_anything(url:str):
+    """"""
+    scraper = Scrape(url)
+    scraper.doit()
+    print(scraper.bs)
+
+if __name__ == "__main__":
+    exit(cli())
