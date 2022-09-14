1593724f9c857f1f56e14aad10f853333f8a2984
Chris Lee <chris@noreply.github.com>
2022-09-13 21:56:59 -0400

    I modified scrape to write the html doc locally.

--- src/landbank_viz/__main__.py
+++ src/landbank_viz/__main__.py
@@ -9,7 +9,7 @@ def scrape_anything(url:str):
     """"""
     scraper = Scrape(url)
     scraper.doit()
-    print(scraper.bs)
+    #print(scraper.bs)
 
 if __name__ == "__main__":
     exit(cli())
--- src/landbank_viz/scrape.py
+++ src/landbank_viz/scrape.py
@@ -4,6 +4,7 @@ bar
 
 import bs4
 import requests
+import os
 
 class Scrape:
     """ foo """
@@ -11,7 +12,21 @@ class Scrape:
         """"""
         self.url = url
 
+    def get_html(self):
+        """write html file locally"""
+        if not os.path.exists("html_file"):
+            print("---Writing html file locally----")
+            response = requests.get(self.url)
+            data =  "".join([line for line in response.text])
+            with open("html_file", "w") as file:
+                for line in data:
+                    file.write(line)
+        else:
+            print("----'html_doc' already exists----")
+
+
     def doit(self):
         """it does it"""
-        response = requests.get(self.url)
-        self.bs = bs4.BeautifulSoup(response.content)
+        #response = requests.get(self.url)
+        self.get_html()
+        #self.bs = bs4.BeautifulSoup(response.content)
