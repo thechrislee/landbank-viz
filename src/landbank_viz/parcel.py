5a862e75698da6e6b835f77166642310c935b9f4
JnyJny <erik.oshaughnessy@gmail.com>
2022-09-18 12:02:59 -0500

    Updated Parcel class - removed available classmethod - removed oneline_address str property - removed coordinates tuple property - removed address dict property - added inventory classmethod - added address str property - added is_available bool property - changed: __str__ prints parcel_id and address
    The Parcel class previously called a geocoder API in the coordinates
    property. This increases the latency for determining the coordinates
    for group of parcels substantially. Geocoding also violates seperation
    of concerns for the Parcel class, meaning parcel is too entangled with
    the gecoding API. Problems arise when geocoding changes and then
    parcel has to change to track those changes. This cascading set of
    changes is often referred to as "software fragility".

--- src/landbank_viz/parcel.py
+++ src/landbank_viz/parcel.py
@@ -5,14 +5,14 @@ from dataclasses import dataclass
 
 from loguru import logger
 
-from .geocode import CensusGeocode
+
 from .scrape import AvailableScraper, ParcelIdScraper
 
 
 @dataclass
 class Parcel:
     parcel_id: str
-    unit: str
+    number: str
     street: str
     city: str
     ward: str
@@ -20,57 +20,18 @@ class Parcel:
     status: str
 
     @classmethod
-    def available(cls, url: str) -> list["Parcel"]:
-        return [
-            cls(**info)
-            for info in AvailableScraper(url).parcels
-            if not "not available" in info["status"].lower()
-        ]
-
-    @property
-    def oneline_address(self) -> str:
-        try:
-            return self._address
-        except AttributeError:
-            pass
-
-        # self._address = ParcelIdScraper(self.parcel_id).address
-        self._address = " ".join([self.unit, self.street, self.city, "OH"])
-
-        return self._address
-
-    @property
-    def address(self) -> dict:
-        try:
-            return self._address
-        except AttributeError:
-            pass
-
-        self._address: dict[str, str] = {
-            "id": self.parcel_id,
-            "street": f"{self.unit} {self.street}",
-            "city": self.city,
-            "state": "OH",
-            "zip": "",
-        }
-
-        return self._address
-
-    @property
-    def coordinates(self) -> tuple[float, float]:
-        try:
-            return self._coordinates
-        except AttributeError:
-            pass
-        try:
-            self._coordinates: tuple[
-                float, float
-            ] = CensusGeocode.coordinates_for_address(self.oneline_address)
-        except Exception as error:
-            logger.error(f"{self.address} {error}")
-            self._coordinates = "Not Available"
-        return self._coordinates
+    def inventory(cls, url: str) -> list["Parcel"]:
+        return [cls(**info) for info in AvailableScraper(url).parcels]
 
     def __str__(self) -> str:
+        return f"{self.parcel_id} {self.address}"
 
-        return f"{self.parcel_id} {self.oneline_address} {self.coordinates}"
+    @property
+    def is_available(self) -> bool:
+        return "not available" not in self.status.lower()
+
+    @property
+    def address(self) -> str:
+        """The street address for this parcel."""
+
+        return f"{self.number} {self.street}, {self.city}"
