1c67e6350fb779138168850986dc270246762e4f
JnyJny <erik.oshaughnessy@gmail.com>
2022-09-18 12:16:39 -0500

    Updated __main__.scrape_anything method - removed call to Parcel.available classmethod - removed per-parcel coordinate lookup - added call to Parcel.inventory - added geocode.CensusGeocode import - added bulk lookup up of aggregated parcel addresses
    By removing the per-parcel coordinate lookup and replacing with
    a bulk lookup, we reduced the number of HTTP roundtrips from 1+NPARCELS
    to 2 (scrape available inventory, bulk lookup). This significantly
    reduces the amount of time to build a list of addresses and their
    geographic coordinates.

--- src/landbank_viz/__main__.py
+++ src/landbank_viz/__main__.py
@@ -3,6 +3,7 @@ import typer
 
 from loguru import logger
 
+from .geocode import CensusGeocode
 from .parcel import Parcel
 
 cli = typer.Typer()
@@ -14,10 +15,17 @@ def scrape_anything(url: str = None):
 
     url = url or "https://cuyahogalandbank.org/all-available-properties/"
 
-    parcels = Parcel.available(url)
+    parcels = [parcel for parcel in Parcel.inventory(url) if parcel.is_available]
 
-    for parcel in parcels:
-        print(parcel)
+    geocoder = CensusGeocode()
+
+    results = geocoder.lookup_addresses([p.address for p in parcels])
+
+    for entry in results:
+        if not entry.is_match:
+            logger.info(f"Not a match: {entry}")
+            continue
+        logger.info(f"Match {entry.parsed} {entry.coordinates}")
 
 
 if __name__ == "__main__":
